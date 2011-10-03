fbd871a80ad618e6bce6c28f79da03c715c11055
Philip Butler <philipebutler@gmail.com>
2011-10-02 20:58:29 -0500

    Added ALFA and CDCL card types.

---
+++ EpplerIO/Cards/CDCL.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace EpplerIO.Cards
+{
+    class CDCL : EpplerCard
+    {
+
+        private WordSeries words;
+
+        public void CDCL()
+        {
+            this.init();
+        }
+
+        public void CDCL(string cardString)
+        {
+        }
+
+        private void init()
+        {
+            CardType = EpplerCommon.CardType.BoundaryLayerOptions;
+            Name = "CDCL";
+            Description = "Initiates the plotting of the boundary-layer summary: lift, drag, and pitching moment.";
+            words = new WordSeries();
+        }
+
+        new public string ToString()
+        {
+            return base.ToString();
+        }
+
+        new public  bool MultipleAllowed
+        {
+            get
+            {
+                return false;
+            }
+        }
+
+        public void Load(string card)
+        {
+
+        }
+    }
+}
